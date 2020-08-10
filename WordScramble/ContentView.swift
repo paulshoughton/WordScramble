f3b7996f2e6dd3d80ef2affff2c5ede9343a59d2
Paul Houghton <paul.houghton@gmail.com>
2020-08-10 08:52:03 +0100

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Paul Houghton on 10/08/2020.
+//  Copyright © 2020 Paul Houghton. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
